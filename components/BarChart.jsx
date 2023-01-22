9edae9b0a5473a7f4570a8122c4b89fabf2ecfc0
Jkhall81 <jason.kei.hall@gmail.com>
2023-01-21 21:25:25 -0700

    started adding hooks to barchart component

--- components/BarChart.jsx
+++ components/BarChart.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const BarChart = () => {
-  return <div>BarChart</div>;
+  const [chartData, setChartDate] = useState({
+    datasets: [],
+  });
+
+  const [chartOptions, setChartOptions] = useState({});
+
+  return (
+    <>
+      <div className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white">
+        <Bar data={chartData} options={chartOptions} />
+      </div>
+    </>
+  );
 };
 
 export default BarChart;
